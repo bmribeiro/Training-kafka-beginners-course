dc12d4ab37cfd98040792473b0444522c08b79a1
USERSAD\bmiguelr <bruno.miguel.ribeiro@emeal.nttdata.com>
2023-03-28 16:50:20 +0100

    Java Consumer

---
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemo.java
@@ -0,0 +1,67 @@
+package io.conduktor.demos.kafka;
+
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Consumer");
+
+        String groupId = "my-java-application";
+        String topic = "demo_java";
+
+        // create Producer Properties
+        Properties properties = new Properties();
+
+        // Localhost
+        // properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // Conduktor
+
+        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"3fvxoDa7e3C4eqPnskpWrZ\" password=\"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY29uZHVrdG9yLmlvIiwic291cmNlQXBwbGljYXRpb24iOiJhZG1pbiIsInVzZXJNYWlsIjpudWxsLCJwYXlsb2FkIjp7InZhbGlkRm9yVXNlcm5hbWUiOiIzZnZ4b0RhN2UzQzRlcVBuc2twV3JaIiwib3JnYW5pemF0aW9uSWQiOjcxNzAzLCJ1c2VySWQiOjgzMTYyLCJmb3JFeHBpcmF0aW9uQ2hlY2siOiIwMTYwYTQxYy0yOTQ3LTRhYjEtOWYyNC0xMmNjNmI4MDk4N2IifX0.eVck5TzS0JfDZptBZvAEu-Dmcd9WwLbQzQMGE717iiI\";");
+        properties.setProperty("sasl.mechanism", "PLAIN");
+
+        // create consumer configs
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+
+        properties.setProperty("group.id", groupId);
+
+        properties.setProperty("auto.offset.reset", "earliest");
+
+        // create a consumer
+        KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(properties);
+
+        consumer.subscribe(Arrays.asList(topic));
+
+        while (true){
+
+            log.info("Polling");
+
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+
+            for(ConsumerRecord<String, String> record : records){
+                log.info("Key: " + record.key() + ", Value: " + record.value());
+                log.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+            }
+        }
+
+    }
+}
